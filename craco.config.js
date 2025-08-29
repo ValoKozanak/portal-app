4f11cd31693a89ef9a53038d701489552633c80e
ValoKozanak <kozanak@dividenda.sk>
2025-08-29 17:58:26 +0200

    Simplify polyfills to avoid build issues

--- craco.config.js
+++ craco.config.js
@@ -9,23 +9,7 @@ module.exports = {
           "stream": require.resolve("stream-browserify"),
           "buffer": require.resolve("buffer"),
           "util": require.resolve("util"),
-          "process": require.resolve("process/browser"),
-          "path": require.resolve("path-browserify"),
-          "os": require.resolve("os-browserify/browser"),
-          "querystring": require.resolve("querystring-es3"),
-          "url": require.resolve("url"),
-          "assert": require.resolve("assert"),
-          "constants": require.resolve("constants-browserify"),
-          "domain": require.resolve("domain-browser"),
-          "events": require.resolve("events"),
-          "http": require.resolve("http-browserify"),
-          "https": require.resolve("https-browserify"),
-          "punycode": require.resolve("punycode"),
-          "string_decoder": require.resolve("string_decoder"),
-          "timers": require.resolve("timers-browserify"),
-          "tty": require.resolve("tty-browserify"),
-          "vm": require.resolve("vm-browserify"),
-          "zlib": require.resolve("zlib")
+          "process": require.resolve("process/browser")
         }
       }
     },
@@ -37,8 +21,5 @@ module.exports = {
         }),
       ]
     }
-  },
-  devServer: {
-    allowedHosts: 'all'
   }
 };
