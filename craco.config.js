205e73ef1e776ae7d08b0ec5a23e41c5c4defd64
ValoKozanak <kozanak@dividenda.sk>
2025-08-29 17:52:27 +0200

    Add all Node.js polyfills for dropbox compatibility

--- craco.config.js
+++ craco.config.js
@@ -7,7 +7,25 @@ module.exports = {
         fallback: {
           "crypto": require.resolve("crypto-browserify"),
           "stream": require.resolve("stream-browserify"),
-          "buffer": require.resolve("buffer")
+          "buffer": require.resolve("buffer"),
+          "util": require.resolve("util"),
+          "process": require.resolve("process/browser"),
+          "path": require.resolve("path-browserify"),
+          "os": require.resolve("os-browserify/browser"),
+          "querystring": require.resolve("querystring-es3"),
+          "url": require.resolve("url"),
+          "assert": require.resolve("assert"),
+          "constants": require.resolve("constants-browserify"),
+          "domain": require.resolve("domain-browser"),
+          "events": require.resolve("events"),
+          "http": require.resolve("http-browserify"),
+          "https": require.resolve("https-browserify"),
+          "punycode": require.resolve("punycode"),
+          "string_decoder": require.resolve("string_decoder"),
+          "timers": require.resolve("timers-browserify"),
+          "tty": require.resolve("tty-browserify"),
+          "vm": require.resolve("vm-browserify"),
+          "zlib": require.resolve("zlib")
         }
       }
     },
@@ -15,6 +33,7 @@ module.exports = {
       add: [
         new webpack.ProvidePlugin({
           Buffer: ['buffer', 'Buffer'],
+          process: 'process/browser',
         }),
       ]
     }
