be5d2bba008a044be8ef7d1425f7ebd6a21da1bf
ValoKozanak <kozanak@dividenda.sk>
2025-09-02 15:56:13 +0200

    Fix axios compatibility: downgrade to 0.27.2 and improve webpack polyfills

--- craco.config.js
+++ craco.config.js
@@ -2,23 +2,37 @@ const webpack = require('webpack');
 
 module.exports = {
   webpack: {
-    configure: {
-      resolve: {
-        fallback: {
-          "crypto": require.resolve("crypto-browserify"),
-          "util": require.resolve("util/"),
-          "buffer": require.resolve("buffer/"),
-          "process": require.resolve("process/browser"),
-          "stream": require.resolve("stream-browserify"),
-          "path": false,
-          "os": false,
-          "fs": false,
-          "vm": false
-        }
-      }
-    },
-    plugins: {
-      add: [
+    configure: (webpackConfig) => {
+      // Add fallbacks for Node.js core modules
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        "crypto": require.resolve("crypto-browserify"),
+        "util": require.resolve("util/"),
+        "buffer": require.resolve("buffer/"),
+        "process": require.resolve("process/browser"),
+        "stream": require.resolve("stream-browserify"),
+        "path": false,
+        "os": false,
+        "fs": false,
+        "vm": false,
+        "querystring": false,
+        "url": false,
+        "http": false,
+        "https": false,
+        "zlib": false,
+        "assert": false,
+        "constants": false,
+        "domain": false,
+        "events": false,
+        "punycode": false,
+        "string_decoder": false,
+        "sys": false,
+        "timers": false,
+        "tty": false
+      };
+
+      // Add plugins
+      webpackConfig.plugins.push(
         new webpack.ProvidePlugin({
           Buffer: ['buffer', 'Buffer'],
           process: 'process/browser',
@@ -28,8 +42,12 @@ module.exports = {
           'process.version': JSON.stringify(process.version),
           'process.platform': JSON.stringify(process.platform),
           'process.arch': JSON.stringify(process.arch),
+          'process.browser': true,
+          'process.node': false
         })
-      ]
+      );
+
+      return webpackConfig;
     }
   }
 };
